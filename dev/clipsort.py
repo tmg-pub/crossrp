0f519c799f2539526731f216efc4779bd09c0f33
tmg <tmg@clubtammy.info>
2021-01-03 16:31:32 -0600

    Rename dev

--- dev/clipsort.py
+++
@@ -1,7 +0,0 @@
-from Tkinter import Tk
-r = Tk()
-r.withdraw()
-r.clipboard_clear()
-r.clipboard_append('i can has clipboardz?')
-r.update() # now it stays on the clipboard after the window is closed
-r.destroy()
